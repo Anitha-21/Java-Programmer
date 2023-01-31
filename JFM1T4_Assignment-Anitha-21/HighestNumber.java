a12438bdbbf2d148613a9de501dde17aabdc1618
Navundru Anitha <116826904+Anitha-21@users.noreply.github.com>
2023-01-31 21:15:34 +0530

    Add files via upload

---
+++ JFM1T4_Assignment-Anitha-21/HighestNumber.java
@@ -0,0 +1,57 @@
+/* JFM1T4_Assignment1:
+   Write a program to find the highest among the given 3 numbers.
+   Prompt the user for the 3 numbers to be input from the terminal.
+
+   SampleInput: 
+   Enter the first number: 11
+   Enter the second number: 22
+   Enter the third number: 1
+
+   Expected Output:
+   22 is highest number
+ 	
+*/
+
+//import statements for java program to read inputs using Scanner class 
+import java.util.*;
+
+public class HighestNumber {
+  public static void main(String args[])
+  {
+    int a,b,c;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("the values of a, b, c is ");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=sc.nextInt();
+    if(a>b && a>c)
+    {
+      System.out.println(a+" the highest number");
+    }
+    if(b>c && b>a)
+    {
+      System.out.println(b+" the highest number");
+    }
+    if(c>b && c>a)
+    {
+      System.out.println(c+" the highest number");
+    }
+  }
+     
+//Add main method
+
+//Declare the three variables
+
+//Use the scanner class to provide input at execution time
+/* 
+ Scanner s= new Scanner(System.in);
+ System.out.println("Enter the first number");
+ number1=s.nextInt();
+ 
+*/
+
+//check which number is highest using if else condition
+
+//Print the highest of three numbers
+ 
+}
---
+++ JFM1T4_Assignment-Anitha-21/PrimeNumbers.java
@@ -0,0 +1,76 @@
+/* JFM1T4_Assignment3:
+   Write a program to print all the prime numbers between 2 numbers.
+   Prompt the user to enter the 2 numbers.
+   
+   Sample Input:
+   Enter starting Number: 20
+   Enter ending Number: 30
+
+   Expected Output:
+   Prime numbers between 20 and 30 are : 
+   23
+   29
+
+*/
+
+//import statements for java program to read inputs using Scanner class 
+import java.util.*;
+
+public class PrimeNumbers
+  {
+   public static void main(String args[])
+    {
+      int i,num1,num2,count=0,temp=0;
+      Scanner sc=new Scanner(System.in);
+      System.out.println("the starting numbers are ");
+      num1=sc.nextInt();
+      System.out.println("enter the num2 number ");
+      num2=sc.nextInt();
+      if (num1 > num2) {
+      temp = num2;
+      num1 = num2;
+      num2 = temp;
+   }
+   while (num1 < num2) {
+      count = 0;
+      if (num1 <= 1) {
+         ++num1;
+         continue;
+      }
+
+      for (i = 2; i <= num1 / 2; ++i) {
+         if (num1% i == 0) {
+            count = 1;
+            break;
+         }
+      }
+      if (count == 0)
+         System.out.println("the prime numbers are " +num1);
+      ++num1;
+
+          }
+       }
+   }
+//Add main method here
+
+//Declare variables num1, num2, i, count and initialize count as zero
+
+//Use the scanner class to provide input at execution time using scanner object
+//Scanner s= new Scanner(System.in);
+
+/*
+Take start value as input and read using scanner object
+System.out.print("Enter Starting Number : ");
+int start = sc.nextInt();
+*/
+
+//Take end value as an input and read using scanner object
+
+//check for all the integer numbers which is between the two numbers given, using for loop
+
+/*Inside the loop, check if the number can be divided by other numbers. You will need another loop for this.
+  If it is divisble by other numbers (except 1 and itself), then move on to check the next number
+  Else, print the prime number and then move on to the next number
+*/
+ 
+     
