73b808e01139a7802c7f2874903297aacea93f9a
Navundru Anitha <116826904+Anitha-21@users.noreply.github.com>
2023-01-31 21:19:38 +0530

    Add files via upload

---
+++ JFM1T11_Assignment-Anitha-21/FindNumberInArray.java
@@ -0,0 +1,66 @@
+/* JFM1T11_Assignment1:
+
+   Write a program to find a number in an integer array. If found print the number and the array index at which it is found. 
+   Prompt the user input from the terminal.
+   
+   Sample Input: 2,4,6,1,7,8,9
+   Enter number to search x= 9
+
+   Expected Output: 9 found at index 6
+   
+   Enter number to search x= 12
+   Expected Output: Sorry! 12 is not found in array.
+   
+*/
+
+import java.util.Scanner;
+
+public class FindNumberInArray 
+{
+  public static void main(String args[])
+  {
+   int n;          
+    Scanner sc=new Scanner(System.in);
+  System.out.print("Enter total number of elements: ");
+  n=sc.nextInt();
+  int arr[]=new int[n];
+  System.out.println("Enter array elements:");
+    for(int i=0; i<n; i++)
+    {
+     System.out.print("Enter element (" + (i+1) +"): ");
+       arr[i]=sc.nextInt();
+    }
+      int num;
+       System.out.print("Enter number to search: ");
+       num=sc.nextInt();
+        int index=-1;       
+       for(int i=0;i<n;i++)
+       {
+         if(arr[i]==num)
+         {
+          index=i;
+         break;
+        }
+     }
+      if(index==-1)
+      {
+   System.out.println("Sorry! " + num + " is not found in array.");
+    }
+    else
+      {
+    System.out.println(num + " found at index " + index);
+      }
+  }
+}
+
+//main method
+
+//declaring variables
+
+//creating Scanner object
+
+//take input from user
+
+//find number in array
+
+//search the element if found print that position else print not found message
---
+++ JFM1T11_Assignment-Anitha-21/InsertionSort.java
@@ -0,0 +1,68 @@
+/* JFM1T11_Assignment3:
+
+   Have you ever arranged the deck of cards, or maybe shirts in your cupboard? What is common between those two things? 
+   Well, you put the next card or shirt into their proper position, or we can say you insert the next element in its 
+   proper position. Write a program to sort an unsorted array of numbers in the same manner.
+   Prompt the user input from the terminal.
+
+   SampleInput: 6,5,4,3,2,1
+   
+   Expected Output: 1,2,3,4,5,6
+
+*/
+
+import java.util.*;
+
+public class InsertionSort 
+{
+  static void insert(int arr[])
+    {
+      int i,j,key;
+      for(i=1;i<arr.length;i++)
+       {
+        key=arr[i];
+        j=i-1;
+        while(j>=0 && key<=arr[j])
+        {
+          arr[j+1]=arr[j];
+          j=j-1;
+        }
+         arr[j+1]=key;       
+        }
+    }
+    public static void main(String args[])
+    {
+      int arr[]={6,5,4,3,2,1};
+      System.out.println("Elements before sorting:");
+      for(int i=0;i<arr.length;i++)
+      System.out.print(arr[i]+" ");
+      System.out.println();
+      insert(arr);
+      System.out.println("Elements after sorting:");
+      for(int i=0;i<arr.length;i++)
+        {
+       System.out.print(arr[i]+" ");
+        }
+    }
+  }
+//main method
+
+//take input from user
+
+//create integer array for all elements
+
+//call sort method
+
+//print sorted array
+
+//create sort method
+
+//declare variables
+
+// storing current element whose left side is checked for its correct position 
+
+//check whether the adjacent element in left side is greater or less than the current element.
+
+// move the left side element to one position forward.
+
+ // move current element to its  correct position.
