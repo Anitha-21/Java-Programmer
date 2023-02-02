427d6ef043f77b1f8f7f2623662b135ef0ca7b00
Navundru Anitha <116826904+Anitha-21@users.noreply.github.com>
2023-02-02 12:24:45 +0530

    Add files via upload

---
+++ ExceptionHandlingOverview_Assignment-Anitha-21/ArithmeticException.java
@@ -0,0 +1,18 @@
+/* 1. Write a program which will illustrate ArthmeticException. Create a parameterized method which can take two values and print division of two numbers. 
+Please write your comments regarding ArithmeticException such as "When ArithmeticException will occur in this program?"
+*/
+public class ArithmeticException 
+{
+ void division(int a,int b)
+  {
+    int c=a/b;
+  System.out.println("Division is done on the a and b values");
+     System.out.println("Value division: "+c);
+   }
+  public static void main(String[] args)
+   {
+   ArithmeticException ae=new ArithmeticException();
+//Any value is divisible by 0, it will throw the ArthmeticEcception 
+    ae.division(10,0);
+   }
+}
